b4facd9d5409d53468f879eaec744e5b0d64f5d6
ayxan65 <memmedliayxan0@gmail.com>
2024-07-30 23:02:16 +0400

    changes

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -7,9 +7,9 @@ const Card = () => {
 
  
   return (
-    <div className='flex flex-wrap gap-12'>
+    <div className='m-auto  grid grid-cols-1 md:gap-8 md:grid-cols-2 lg:gap-12 lg:grid-cols-5'>
       {head.map(({stock, name}, index) => (
-        <div key={index} className='flex gap-2 h-[10em] border rounded-sm w-[15em]'>
+        <div key={index} className='flex gap-2 h-[10em] shadow-lg w-[15em] my-2 lg:my-0 '>
           
           <p className='text-red text-xl font-semibold'>{name}</p>
           <p>{stock}</p>
